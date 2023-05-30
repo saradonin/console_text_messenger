d7684443be338b25a232b74eb371aa1f5174b490
saradonin <m.jemieljanczuk@gmail.com>
2023-05-30 22:43:02 +0200

    load user added

--- models.py
+++ models.py
@@ -25,9 +25,23 @@ class User:
     def save_to_db(self, cursor):
         if self._id == -1:
             sql = """INSERT INTO users(username, hashed_password)
-                    VALUES (%s, %s) RRETURNING id"""
+                    VALUES (%s, %s) RETURNING id"""
             values = (self.username, self._hashed_password)
             cursor.execute(sql, values)
-            self._id = cursor.fetchone()['id']
+            self._id = cursor.fetchone()[0]
             return True
         return False
+
+    @staticmethod
+    def load_user_by_id(cursor, id_):
+        sql = "SELECT id, username, hashed_password FROM users WHERE id=%s"
+        cursor.execute(sql, (id_,))  # (id_, ) - cause we need a tuple
+        data = cursor.fetchone()
+        if data:
+            id_, username, hashed_password = data
+            loaded_user = User(username)
+            loaded_user._id = id_
+            loaded_user._hashed_password = hashed_password
+            return loaded_user
+        else:
+            return None
