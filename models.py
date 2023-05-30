527e5ac0cab0f357d2fa4855e9bfc981bcba22fc
saradonin <m.jemieljanczuk@gmail.com>
2023-05-30 22:28:42 +0200

    save to db added

--- models.py
+++ models.py
@@ -21,3 +21,13 @@ class User:
     @hashed_password.setter
     def hashed_password(self, password):
         self.set_password(password)
+
+    def save_to_db(self, cursor):
+        if self._id == -1:
+            sql = """INSERT INTO users(username, hashed_password)
+                    VALUES (%s, %s) RRETURNING id"""
+            values = (self.username, self._hashed_password)
+            cursor.execute(sql, values)
+            self._id = cursor.fetchone()['id']
+            return True
+        return False
