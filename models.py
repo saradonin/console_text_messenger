9af23fea942d38d839df8231b57c7af30cf3ce38
saradonin <m.jemieljanczuk@gmail.com>
2023-05-30 22:24:06 +0200

    setter added

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 from clcrypto import hash_password
 
+
 class User:
-    def __init__(self,username, password, salt):
+    def __init__(self, username="", password="", salt=""):
         self._id = -1
         self.username = username
         self._hashed_password = hash_password(password, salt)
@@ -14,4 +15,9 @@ class User:
     def hashed_password(self):
         return self._hashed_password
 
+    def set_password(self, password, salt=""):
+        self._hashed_password = hash_password(password, salt)
 
+    @hashed_password.setter
+    def hashed_password(self, password):
+        self.set_password(password)
